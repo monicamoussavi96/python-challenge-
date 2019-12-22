f6a3e674accfb4c7c345a70e274d5b2a0c5cfe1b
Monica <moussavimonica@gmail.com>
2019-12-21 20:28:43 -0600

    FINAL

--- **PyPoll**/main.py
+++ **PyPoll**/main.py
@@ -1,69 +1,86 @@
 #Import Modules 
 import os
 import csv 
-#Set Variables 
+
 #Set Path for CSV file
 csvpath = os.path.join ("**PyPoll**","election_data.csv")
 
-# Variables
-total_votes = 0
-khan_votes = 0
-correy_votes = 0
-li_votes = 0
-otooley_votes = 0
+# Set Variables 
+totalVotes = 0
+khanVotes = 0
+correyVotes = 0
+liVotes = 0
+otooleyVotes = 0
 
 # Open & Read CSV File
 with open(csvpath, newline='') as csvfile:
-
-    # CSV Reader Specifies Delimiter & Variable That Holds Contents
     csvreader = csv.reader(csvfile, delimiter=',')
     
-    # Read The Header Row First (Skip This Step If There Is No Header)
+    # Read The Header Row 
     csv_header = next(csvfile)
 
     # Read Each Row Of Data After The Header
     for row in csvreader:
         
         # Calculate Total Number Of Votes Cast
-        total_votes += 1
+        totalVotes += 1
         
         # Calculate Total Number Of Votes Each Candidate Won
         if (row[2] == "Khan"):
-            khan_votes += 1
+            khanVotes += 1
         elif (row[2] == "Correy"):
-            correy_votes += 1
+            correyVotes += 1
         elif (row[2] == "Li"):
-            li_votes += 1
+            liVotes += 1
         else:
-            otooley_votes += 1
+            otooleyVotes += 1
             
     # Calculate Percentage Of Votes Each Candidate Won
-    kahn_percent = khan_votes / total_votes
-    correy_percent = correy_votes / total_votes
-    li_percent = li_votes / total_votes
-    otooley_percent = otooley_votes / total_votes
+    kahnPercent = khanVotes / totalVotes
+    correyPercent = correyVotes / totalVotes
+    liPercent = liVotes / totalVotes
+    otooleyPercent = otooleyVotes / totalVotes
     
     # Calculate Winner Of The Election Based On Popular Vote
-    winner = max(khan_votes, correy_votes, li_votes, otooley_votes)
+    winner = max(khanVotes, correyVotes, liVotes, otooleyVotes)
 
-    if winner == khan_votes:
+    if winner == khanVotes:
         winner_name = "Khan"
-    elif winner == correy_votes:
+    elif winner == correyVotes:
         winner_name = "Correy"
-    elif winner == li_votes:
+    elif winner == liVotes:
         winner_name = "Li"
     else:
         winner_name = "O'Tooley" 
 
-# Print Analysis
+# Print 
 print(f"Election Results")
 print(f"---------------------------")
-print(f"Total Votes: {total_votes}")
+print(f"Total Votes: {totalVotes}")
 print(f"---------------------------")
-print(f"Kahn: {kahn_percent:.3%}({khan_votes})")
-print(f"Correy: {correy_percent:.3%}({correy_votes})")
-print(f"Li: {li_percent:.3%}({li_votes})")
-print(f"O'Tooley: {otooley_percent:.3%}({otooley_votes})")
+print(f"Kahn: {kahnPercent:.3%}({khanVotes})")
+print(f"Correy: {correyPercent:.3%}({correyVotes})")
+print(f"Li: {liPercent:.3%}({liVotes})")
+print(f"O'Tooley: {otooleyPercent:.3%}({otooleyVotes})")
 print(f"---------------------------")
 print(f"Winner: {winner_name}")
 print(f"---------------------------")
+
+# Specify File To Write To
+output_file = os.path.join("**PyPoll**","FINALPyPoll.text")
+
+# Open File Using "Write" Mode. Specify The Variable 
+with open(output_file, 'w',) as txt:
+
+# Write data into text file 
+    txt.write(f"Election Results\n")
+    txt.write(f"---------------------------\n")
+    txt.write(f"Total Votes: {totalVotes}\n")
+    txt.write(f"---------------------------\n")
+    txt.write(f"Kahn: {kahnPercent:.3%}({khanVotes})\n")
+    txt.write(f"Correy: {correyPercent:.3%}({correyVotes})\n")
+    txt.write(f"Li: {liPercent:.3%}({liVotes})\n")
+    txt.write(f"O'Tooley: {otooleyPercent:.3%}({otooleyVotes})\n")
+    txt.write(f"---------------------------\n")
+    txt.write(f"Winner: {winner_name}\n")
+    txt.write(f"---------------------------\n")

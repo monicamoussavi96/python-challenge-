f6672a805c6a6efafb18fce9b64354669ba12fff
Monica <moussavimonica@gmail.com>
2019-12-21 19:53:16 -0600

    FINAL

--- **PyBank**/main.py
+++ **PyBank**/main.py
@@ -5,7 +5,7 @@ import csv
 #Set Path for CSV file
 csvpath = os.path.join ("**PyBank**","budget_data.csv")
 
-#initializing the variables 
+#Determine Variables 
 totalMonth = 0
 netAmount =0
 changes =[]
@@ -15,12 +15,12 @@ gIncMonth = 0
 gDec = 0
 gDecMonth = 0
 
-# Open the CSV
+#Open and Read the CSV file
 with open(csvpath, newline = '') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
     next(csvreader, None)
     row = next(csvreader)
-# calculating the total number of months and total revenue
+#Calcuate the total number of months and total revenue
     previousProfit = int(row[1])
     totalMonth = totalMonth + 1
     netAmount = netAmount + int(row[1])
@@ -38,23 +38,23 @@ with open(csvpath, newline = '') as csvfile:
         previousProfit = int(row[1])
         monthlyChange.append(row[0])
         
-        #calculating the greatest increase
+        #Calculate Greatest Increase 
         if int(row[1]) > gInc:
             gInc = int(row[1])
             gIncMonth = row[0]
             
-        #calculating the greatest decrease
+        #Calculate Greatest Decrese
         if int(row[1]) < gDec:
             gDec = int(row[1])
             gDecMonth = row[0]  
       
-    # calculating the average and date
+    #Calculate Average and the date 
     averageChange = sum(changes)/len(changes)
 
     high = max(changes)
     low = min(changes)
 
-    # printing all values
+    #Print Values 
     print(f"Financial Analysis")
     print(f"-----------------------------------")
     print(f"Total Months: {totalMonth}")
@@ -62,3 +62,18 @@ with open(csvpath, newline = '') as csvfile:
     print(f"Average Change: ${averageChange: .2f}")
     print(f"Greatest Increse in Profit:, {gIncMonth}, (${max(changes)})")
     print(f"Greatest Decrease in Profit:, {gDecMonth}, (${min(changes)})")
+
+    # Specify File To Write To
+    output_file = os.path.join("**PyBank**","FINALPyBank.text")
+
+    # Open File Using "Write" Mode. Specify The Variable To Hold The Contents
+    with open(output_file, 'w',) as txt:
+
+# Write New Data
+        txt.write(f"Financial Analysis\n")
+        txt.write(f"---------------------------\n")
+        txt.write(f"Total Months: {totalMonth}\n")
+        txt.write(f"Total: ${netAmount}\n")
+        txt.write(f"Average Change: ${averageChange}\n")
+        txt.write(f"Greatest Increase in Profits:, {gIncMonth}, (${max(changes)})\n")
+        txt.write(f"Greatest Decrease in Profits:, {gDecMonth}, (${min(changes)})\n")
